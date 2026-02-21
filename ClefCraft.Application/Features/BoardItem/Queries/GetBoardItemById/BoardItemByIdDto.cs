748b67649df82fcf7423b5779b1702b722086964
Mete <metekaba96@gmail.com>
2026-02-21 18:09:29 +0300

    improve loading and updating of related entities

--- ClefCraft.Application/Features/BoardItem/Queries/GetBoardItemById/BoardItemByIdDto.cs
+++ ClefCraft.Application/Features/BoardItem/Queries/GetBoardItemById/BoardItemByIdDto.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ClefCraft.Application.Features.Priority.Queries.GetPriorities;
+using ClefCraft.Application.Features.Status.Queries.GetStatuses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,12 +13,12 @@ namespace ClefCraft.Application.Features.BoardItem.Queries.GetBoardItemById
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public int StatusId { get; set; }
-        public string Status { get; set; }
-        public int PriorityId { get; set; }
-        public string Priority { get; set; }
+        public StatusDto? Status { get; set; }
+        public PriorityDto? Priority { get; set; }
         public List<string> Tags { get; set; } = new List<string>();
-        public string Assignee { get; set; }
+        public string AssigneeId { get; set; }
+        public string AssigneeFirstName { get; set; }
+        public string AssigneeLastName { get; set; }
         public DateTime DueDate { get; set; }
         public double EstimatedTime { get; set; } // In hours
         public double TimeSpent { get; set; }
