de6ac3bc35941d6a8a75e3013ba0d2e65ce31860
Mete <metekaba96@gmail.com>
2026-03-31 21:48:33 +0300

    Add New Table: CalendarEventException

---
+++ ClefCraft.Domain/CalendarEventException.cs
@@ -0,0 +1,28 @@
+﻿using ClefCraft.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClefCraft.Domain
+{
+    public class CalendarEventException : BaseEntity
+    {
+        public int CalendarEventId { get; set; }
+
+        // Identifies WHICH occurrence
+        public DateTimeOffset OccurrenceDate { get; set; }
+
+        // Overrides (only applied if not null)
+        public string? Subject { get; set; }
+        public string? Comment { get; set; }
+        public DateTimeOffset? StartDate { get; set; }
+        public DateTimeOffset? EndDate { get; set; }
+
+        // Special cases
+        public bool IsCancelled { get; set; } = false;
+
+        public CalendarEvent CalendarEvent { get; set; }
+    }
+}
