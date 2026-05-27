cba85c551761453131416fbd348c261f00f5b316
Mete <metekaba96@gmail.com>
2026-05-27 21:06:25 +0300

    update: UpdateSingleOccurrenceCommand

--- ClefCraft.Application/Features/Calendar/Commands/UpdateSingleOccurrence/UpdateSingleOccurrenceCommand.cs
+++ ClefCraft.Application/Features/Calendar/Commands/UpdateSingleOccurrence/UpdateSingleOccurrenceCommand.cs
@@ -10,15 +10,27 @@ namespace ClefCraft.Application.Features.Calendar.Commands.UpdateSingleOccurrenc
 {
     public class UpdateSingleOccurrenceCommand : IRequest
     {
-        public int EventId { get; set; }
+        /// <summary>
+        /// Identifies the recurring series (NOT a single event anymore)
+        /// </summary>
+        public string SeriesUid { get; set; }
+
+        /// <summary>
+        /// The exact occurrence being modified
+        /// </summary>
         public DateTimeOffset OccurrenceDate { get; set; }
+
         public string? Subject { get; set; }
         public string? Comment { get; set; }
+
         public DateTimeOffset? StartDate { get; set; }
         public DateTimeOffset? EndDate { get; set; }
+
         public Optional<string?>? Location { get; set; }
+
         public string? Importance { get; set; }
         public int? EventTypeId { get; set; }
+
         public bool? IsCancelled { get; set; }
     }
 }
