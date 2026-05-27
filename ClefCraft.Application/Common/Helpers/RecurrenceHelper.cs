47e9a0029c82cfc3b57aac7bb8488f8f135de6bf
Mete <metekaba96@gmail.com>
2026-05-27 23:35:54 +0300

    fix: RecurrenceHelper

--- ClefCraft.Application/Common/Helpers/RecurrenceHelper.cs
+++ ClefCraft.Application/Common/Helpers/RecurrenceHelper.cs
@@ -8,34 +8,49 @@ namespace ClefCraft.Application.Common.Helpers
 {
     public static class RecurrenceHelper
     {
-        private static CalendarEvent ApplyException(
+        private static CalendarEvent? ApplyException(
             CalendarEvent occurrence,
-            CalendarEvent ev,
+            CalendarEvent sourceEvent,
             List<CalendarEventException> exceptions)
         {
-            var occurrenceDate = DateOnly.FromDateTime(occurrence.StartDate.UtcDateTime);
+            var occurrenceDate =
+                DateOnly.FromDateTime(
+                    occurrence.StartDate.UtcDateTime);
 
             var exception = exceptions.FirstOrDefault(x =>
-                x.SeriesUid == ev.BaseEventId.ToString() &&
-                DateOnly.FromDateTime(x.OccurrenceDate.UtcDateTime) == occurrenceDate);
+                x.SeriesUid == sourceEvent.SeriesUid &&
+                DateOnly.FromDateTime(
+                    x.OccurrenceDate.UtcDateTime) == occurrenceDate);
 
-            if (exception != null)
-            {
-                if (exception.IsCancelled)
-                    return null;
+            if (exception == null)
+                return occurrence;
 
-                occurrence.Subject = exception.Subject ?? occurrence.Subject;
-                occurrence.Comment = exception.Comment ?? occurrence.Comment;
-                occurrence.StartDate = exception.StartDate ?? occurrence.StartDate;
-                occurrence.EndDate = exception.EndDate ?? occurrence.EndDate;
-                occurrence.Location = exception.Location ?? occurrence.Location;
-            }
+            if (exception.IsCancelled)
+                return null;
+
+            occurrence.Subject =
+                exception.Subject ?? occurrence.Subject;
+
+            occurrence.Comment =
+                exception.Comment ?? occurrence.Comment;
+
+            occurrence.StartDate =
+                exception.StartDate ?? occurrence.StartDate;
+
+            occurrence.EndDate =
+                exception.EndDate ?? occurrence.EndDate;
+
+            occurrence.Location =
+                exception.Location ?? occurrence.Location;
+
+            occurrence.EventTypeId =
+                exception.EventTypeId ?? occurrence.EventTypeId;
 
             return occurrence;
         }
 
         public static List<CalendarEvent> ExpandEvent(
-            CalendarEvent ev,
+            CalendarEvent sourceEvent,
             RecurrenceRule rule,
             List<CalendarEventException> exceptions,
             DateTimeOffset rangeStart,
@@ -43,42 +58,75 @@ namespace ClefCraft.Application.Common.Helpers
         {
             var result = new List<CalendarEvent>();
 
-            var current = ev.StartDate;
+            var current = sourceEvent.StartDate;
+
+            var generated = 0;
 
             while (current <= rangeEnd)
             {
+                if (rule.Count.HasValue &&
+                    generated >= rule.Count.Value)
+                {
+                    break;
+                }
+
+                if (rule.EndDate.HasValue &&
+                    current > rule.EndDate.Value)
+                {
+                    break;
+                }
+
                 if (current >= rangeStart)
                 {
+                    var duration =
+                        sourceEvent.EndDate - sourceEvent.StartDate;
+
                     var occurrence = new CalendarEvent
                     {
-                        Id = ev.Id,
-                        BaseEventId = ev.BaseEventId,
-
-                        Subject = ev.Subject,
-                        Location = ev.Location,
-                        Comment = ev.Comment,
-
+                        Id = sourceEvent.Id,
+                        BaseEventId = sourceEvent.Id,
+                        SeriesUid = sourceEvent.SeriesUid,
+                        Subject = sourceEvent.Subject,
+                        Location = sourceEvent.Location,
+                        Comment = sourceEvent.Comment,
                         StartDate = current,
-                        EndDate = current + (ev.EndDate - ev.StartDate),
-
-                        AllDayEvent = ev.AllDayEvent,
-                        EventTypeId = ev.EventTypeId,
-                        Importance = ev.Importance
+                        EndDate = current + duration,
+                        AllDayEvent = sourceEvent.AllDayEvent,
+                        EventTypeId = sourceEvent.EventTypeId,
+                        Importance = sourceEvent.Importance,
+                        IsRecurring = true,
+                        RecurrenceRuleJson =
+                            sourceEvent.RecurrenceRuleJson,
+                        LinkedBoardItemId =
+                            sourceEvent.LinkedBoardItemId
                     };
 
-                    occurrence = ApplyException(occurrence, ev, exceptions);
+                    occurrence = ApplyException(
+                        occurrence,
+                        sourceEvent,
+                        exceptions);
 
                     if (occurrence != null)
+                    {
                         result.Add(occurrence);
+                    }
                 }
+
+                generated++;
 
                 current = rule.Frequency switch
                 {
-                    "DAILY" => current.AddDays(rule.Interval),
-                    "WEEKLY" => current.AddDays(7 * rule.Interval),
-                    "MONTHLY" => current.AddMonths(rule.Interval),
-                    "YEARLY" => current.AddYears(rule.Interval),
-                    _ => throw new NotSupportedException()
+                    "DAILY" =>
+                        current.AddDays(rule.Interval),
+                    "WEEKLY" =>
+                        current.AddDays(7 * rule.Interval),
+                    "MONTHLY" =>
+                        current.AddMonths(rule.Interval),
+                    "YEARLY" =>
+                        current.AddYears(rule.Interval),
+
+                    _ => throw new NotSupportedException(
+                        $"Unsupported frequency: {rule.Frequency}")
                 };
             }
 
