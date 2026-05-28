1d08cc6d213665e94c265320aea2e3c25870c89c
Mete <metekaba96@gmail.com>
2026-05-28 14:27:21 +0300

    add: RecurrenceSeriesRepository

---
+++ ClefCraft.Application/Contracts/Persistence/IRecurrenceSeriesRepository.cs
@@ -0,0 +1,19 @@
+﻿using ClefCraft.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClefCraft.Application.Contracts.Persistence
+{
+    public interface IRecurrenceSeriesRepository
+        : IGenericRepository<RecurrenceSeries>
+    {
+        Task<RecurrenceSeries?> GetBySeriesUidAsync(
+            string seriesUid);
+
+        Task<List<RecurrenceSeries>> GetByUserIdAsync(
+            string userId);
+    }
+}
---
+++ ClefCraft.Persistence/Repositories/RecurrenceSeriesRepository.cs
@@ -0,0 +1,41 @@
+﻿using ClefCraft.Application.Contracts.Persistence;
+using ClefCraft.Domain;
+using ClefCraft.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClefCraft.Persistence.Repositories
+{
+    public class RecurrenceSeriesRepository
+        : GenericRepository<RecurrenceSeries>,
+          IRecurrenceSeriesRepository
+    {
+        public RecurrenceSeriesRepository(
+            ClefCraftDatabaseContext context)
+            : base(context)
+        {
+        }
+
+        public async Task<RecurrenceSeries?> GetBySeriesUidAsync(
+            string seriesUid)
+        {
+            return await _context.Set<RecurrenceSeries>()
+                .Include(x => x.Segments)
+                .FirstOrDefaultAsync(x =>
+                    x.SeriesUid == seriesUid);
+        }
+
+        public async Task<List<RecurrenceSeries>> GetByUserIdAsync(
+            string userId)
+        {
+            return await _context.Set<RecurrenceSeries>()
+                .Include(x => x.Segments)
+                .Where(x => x.UserId == userId)
+                .ToListAsync();
+        }
+    }
+}
