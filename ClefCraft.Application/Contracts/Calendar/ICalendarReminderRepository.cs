05faf6615791322b75e4d6251a8d28fb3db8f2b7
Mete <metekaba96@gmail.com>
2026-06-01 17:37:40 +0300

    add: ICalendarReminderRepository

---
+++ ClefCraft.Application/Contracts/Calendar/ICalendarReminderRepository.cs
@@ -0,0 +1,21 @@
+﻿using ClefCraft.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClefCraft.Application.Contracts.Calendar
+{
+    public interface ICalendarReminderRepository
+    {
+        Task<List<CalendarReminder>> GetByEventIdAsync(
+            int eventId);
+
+        Task CreateAsync(
+            CalendarReminder reminder);
+
+        Task DeleteByEventIdAsync(
+            int eventId);
+    }
+}
