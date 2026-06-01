be57208fc325ff4d363bd8e656d5932723db810f
Mete <metekaba96@gmail.com>
2026-06-01 17:34:33 +0300

    add: CalendarReminder

---
+++ ClefCraft.Domain/CalendarReminder.cs
@@ -0,0 +1,20 @@
+﻿using ClefCraft.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClefCraft.Domain
+{
+    public class CalendarReminder : BaseEntity
+    {
+        public int CalendarEventId { get; set; }
+
+        public CalendarEvent CalendarEvent { get; set; }
+
+        public int MinutesBeforeStart { get; set; }
+
+        public bool IsEnabled { get; set; } = true;
+    }
+}
