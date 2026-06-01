b00ffcde3e66370d89a5517e110e340225c281e9
Mete <metekaba96@gmail.com>
2026-06-01 17:38:13 +0300

    add: INotificationQueueRepository

---
+++ ClefCraft.Application/Contracts/Calendar/INotificationQueueRepository.cs
@@ -0,0 +1,21 @@
+﻿using ClefCraft.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClefCraft.Application.Contracts.Calendar
+{
+    public interface INotificationQueueRepository
+    {
+        Task CreateAsync(
+            NotificationQueue notification);
+
+        Task<List<NotificationQueue>> GetPendingAsync(
+            DateTimeOffset utcNow);
+
+        Task UpdateAsync(
+            NotificationQueue notification);
+    }
+}
