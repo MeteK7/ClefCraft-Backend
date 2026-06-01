e2a2ad21aed24de4c4c2ad75b30062fe6ae9243f
Mete <metekaba96@gmail.com>
2026-06-01 17:35:05 +0300

    add: NotificationQueue

---
+++ ClefCraft.Domain/NotificationQueue.cs
@@ -0,0 +1,24 @@
+﻿using ClefCraft.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClefCraft.Domain
+{
+    public class NotificationQueue : BaseEntity
+    {
+        public string UserId { get; set; }
+
+        public int CalendarEventId { get; set; }
+
+        public DateTimeOffset ScheduledFor { get; set; }
+
+        public bool IsProcessed { get; set; }
+
+        public DateTimeOffset? ProcessedAt { get; set; }
+
+        public string Message { get; set; }
+    }
+}
