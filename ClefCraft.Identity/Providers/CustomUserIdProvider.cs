f46dea4d1ddddd8646e172f25dbcf10bcb0a90d4
Mete <metekaba96@gmail.com>
2026-06-07 22:17:06 +0300

    add: CustomUserIdProvider

---
+++ ClefCraft.Identity/Providers/CustomUserIdProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClefCraft.Identity.Providers
+{
+    public class CustomUserIdProvider : IUserIdProvider
+    {
+        public string? GetUserId(HubConnectionContext connection)
+        {
+            // Extract the user identity id ("uid") instead of the default username string ("sub")
+            return connection.User?.FindFirst("uid")?.Value;
+        }
+    }
+}
