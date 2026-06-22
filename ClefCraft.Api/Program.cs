9c9cdd91d83023794391f7ec5952ee29f4c164b3
Mete <metekaba96@gmail.com>
2026-06-22 23:54:57 +0300

    modify: Program.cs

--- ClefCraft.Api/Program.cs
+++ ClefCraft.Api/Program.cs
@@ -18,6 +18,11 @@ using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(8080);
+});
+
 // Add services to the container.
 
 builder.Host.UseSerilog((context, loggerConfig) => loggerConfig.WriteTo.Console().ReadFrom.Configuration(context.Configuration));
