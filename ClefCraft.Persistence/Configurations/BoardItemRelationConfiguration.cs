39af25ac280c43bac01c8cc96edae1796c7b67d4
Mete <metekaba96@gmail.com>
2026-07-07 23:59:37 +0300

    add: BoardItemRelationConfiguration

--- ClefCraft.Persistence/Configurations/BoardItemRelationConfiguration.cs
+++ ClefCraft.Persistence/Configurations/BoardItemRelationConfiguration.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ClefCraft.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace ClefCraft.Persistence.Configurations
 {
-    internal class BoardItemRelationConfiguration
+    public class BoardItemRelationConfiguration
+        : IEntityTypeConfiguration<BoardItemRelation>
     {
+        public void Configure(EntityTypeBuilder<BoardItemRelation> builder)
+        {
+            builder.ToTable("BoardItemRelations");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.RelationType)
+                   .IsRequired();
+
+            builder.HasOne(x => x.SourceBoardItem)
+                   .WithMany(x => x.OutgoingRelations)
+                   .HasForeignKey(x => x.SourceBoardItemId)
+                   .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(x => x.TargetBoardItem)
+                   .WithMany(x => x.IncomingRelations)
+                   .HasForeignKey(x => x.TargetBoardItemId)
+                   .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasIndex(x => new
+            {
+                x.SourceBoardItemId,
+                x.TargetBoardItemId,
+                x.RelationType
+            })
+            .IsUnique();
+        }
     }
 }
