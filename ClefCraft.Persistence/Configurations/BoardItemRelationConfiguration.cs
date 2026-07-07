05c16aad30079f85aca0bd699307725e6843b70e
Mete <metekaba96@gmail.com>
2026-07-07 23:57:35 +0300

    add: new files for link development.

---
+++ ClefCraft.Persistence/Configurations/BoardItemRelationConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClefCraft.Persistence.Configurations
+{
+    internal class BoardItemRelationConfiguration
+    {
+    }
+}
