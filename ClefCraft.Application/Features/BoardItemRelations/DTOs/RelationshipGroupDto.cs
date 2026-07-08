c753e6365fb4be4e19e60390f08b0f907489edc8
Mete <metekaba96@gmail.com>
2026-07-08 23:00:26 +0300

    add: relationship dtos

---
+++ ClefCraft.Application/Features/BoardItemRelations/DTOs/RelationshipGroupDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClefCraft.Application.Features.BoardItemRelations.DTOs
+{
+    public class RelationshipGroupDto
+    {
+        public int RelationType { get; set; }
+
+        public string Name { get; set; } = "";
+
+        public List<RelationshipCardDto> Items { get; set; }
+            = new();
+    }
+}
