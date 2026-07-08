cfc2b53009e0e26c69225ed3b4eba2b5086aa5d0
Mete <metekaba96@gmail.com>
2026-07-08 21:46:35 +0300

    add: BoardItemRelationRepository

---
+++ ClefCraft.Application/Contracts/Persistence/IBoardItemRelationRepository.cs
@@ -0,0 +1,26 @@
+﻿using ClefCraft.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClefCraft.Application.Contracts.Persistence;
+
+public interface IBoardItemRelationRepository
+{
+    Task<List<BoardItemRelation>> GetForItemAsync(int itemId);
+
+    Task AddAsync(BoardItemRelation relation);
+
+    Task DeleteAsync(int id);
+
+    Task<bool> ExistsAsync(
+        int sourceId,
+        int targetId,
+        int relationType);
+
+    Task<List<BoardItem>> SearchItemsAsync(
+        string search,
+        int excludeItemId);
+}
