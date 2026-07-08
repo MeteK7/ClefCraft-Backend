c5588709c68fc4c34035ea544d4e0aacbe452325
Mete <metekaba96@gmail.com>
2026-07-08 22:53:14 +0300

    add: delete board item relation command handler

---
+++ ClefCraft.Application/Features/BoardItemRelations/Commands/DeleteRelation/DeleteBoardItemRelationCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using ClefCraft.Application.Contracts.Persistence;
+using MediatR;
+
+namespace ClefCraft.Application.Features.BoardItemRelations.Commands.DeleteRelation
+{
+    public class DeleteBoardItemRelationCommandHandler
+        : IRequestHandler<DeleteBoardItemRelationCommand>
+    {
+        private readonly IBoardItemRelationRepository _relationRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteBoardItemRelationCommandHandler(
+            IBoardItemRelationRepository relationRepository,
+            IUnitOfWork unitOfWork)
+        {
+            _relationRepository = relationRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Unit> Handle(
+            DeleteBoardItemRelationCommand request,
+            CancellationToken cancellationToken)
+        {
+            await _relationRepository.DeleteAsync(request.RelationId);
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
