964b708e5ec10ede6595967867a9c4a873a1705d
Mete <metekaba96@gmail.com>
2026-07-08 21:49:31 +0300

    add: BoardItemRelation Dtos

---
+++ ClefCraft.Application/Features/BoardItemRelation/DTOs/BoardItemRelationDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClefCraft.Application.Features.BoardItemRelation.DTOs
+{
+    public class BoardItemRelationDto
+    {
+        public int Id { get; set; }
+
+        public int RelationType { get; set; }
+
+        public int ItemId { get; set; }
+
+        public string Title { get; set; } = "";
+
+        public string Status { get; set; } = "";
+
+        public string Priority { get; set; } = "";
+    }
+}
--- ClefCraft.Persistence/PersistenceServiceRegistration.cs
+++ ClefCraft.Persistence/PersistenceServiceRegistration.cs
@@ -105,7 +105,7 @@ namespace ClefCraft.Persistence
             services.AddScoped<IUserInteractionService, UserInteractionService>();
             services.AddScoped<ITaskLifecycleService, TaskLifecycleService>();
             services.AddScoped<IUnitOfWork, EfUnitOfWork>();
-
+            services.AddScoped<IBoardItemRelationRepository, BoardItemRelationRepository>();
             services.AddScoped<INotificationQueueRepository, NotificationQueueRepository>();
             services.AddScoped<ICalendarReminderRepository, CalendarReminderRepository>();
 
