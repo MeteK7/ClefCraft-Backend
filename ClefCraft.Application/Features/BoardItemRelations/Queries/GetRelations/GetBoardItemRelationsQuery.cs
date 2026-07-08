0b5c0f63c5be7aa1c0f5560bc01b46ea23813b3d
Mete <metekaba96@gmail.com>
2026-07-08 23:03:24 +0300

    refactor: GetBoardItemRelationsQuery

--- ClefCraft.Application/Features/BoardItemRelations/Queries/GetRelations/GetBoardItemRelationsQuery.cs
+++ ClefCraft.Application/Features/BoardItemRelations/Queries/GetRelations/GetBoardItemRelationsQuery.cs
@@ -9,8 +9,13 @@ using System.Threading.Tasks;
 namespace ClefCraft.Application.Features.BoardItemRelations.Queries.GetRelations
 {
     public class GetBoardItemRelationsQuery
-        : IRequest<List<BoardItemRelationDto>>
+        : IRequest<RelationshipHubDto>
     {
         public int ItemId { get; set; }
+
+        public GetBoardItemRelationsQuery(int itemId)
+        {
+            ItemId = itemId;
+        }
     }
 }
