d8367f3219c4fa70777bf12b8a515013ef9811aa
Mete <metekaba96@gmail.com>
2026-09-19 23:43:54 +0300

    remove: blazorui and leave feature

--- ClefCraft.BlazorUI/Contracts/ILeaveAllocationService.cs
+++
@@ -1,9 +0,0 @@
-﻿using ClefCraft.BlazorUI.Services.Base;
-
-namespace ClefCraft.BlazorUI.Contracts
-{
-    public interface ILeaveAllocationService
-    {
-        Task<Response<Guid>> CreateLeaveAllocations(int leaveTypeId);
-    }
-}
--- ClefCraft.BlazorUI/Services/Base/IClient.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ClefCraft.BlazorUI.Services.Base
-{
-    public partial interface IClient
-    {
-        public HttpClient HttpClient { get; }
-    }
-}
